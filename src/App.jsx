187a9b89a155876e4ee0c0c0c0c6b2ddfefce1d6
petar-antic <antic.petar994@gmail.com>
2021-12-23 18:39:02 +0100

    Variables and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from './components/navbar/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <h1>HR App</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import star from './star.svg';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="logo">
+        <img src={star} alt="logo" type="button" />
+        <span>Logo</span>
+      </div>
+      <div className="navLinks">
+        <span>Page one</span>
+        <span>Page two</span>
+        <span>Page three</span>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
