54e2d3c11105308135a8e160df1490e071dfb063
vanja-stojanovic-quantox <vanja.stojanovic@quantoxtechnology.com>
2022-01-12 18:12:28 +0100

    design Company Info

---
+++ src/components/CompanyInfo/CompanyInfo.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import '../../styles/CompanyInfo/CompanyInfo.css';
+//import arrow1Right from './assets/icons/arrow 1 - right.svg';
+
+const CompanyInfo = () => {
+  return (
+    <form className="flex flex-column">
+      <h1>Company Info</h1>
+      <label>Company Name</label>
+      <input placeholder="Company Name" />
+      <label>Company Logo</label>
+      <input className="file-input" type="file" />
+      <div className="flex jc-end">
+        <button className="btn-save">Save</button>
+      </div>
+    </form>
+  );
+};
+
+export default CompanyInfo;
